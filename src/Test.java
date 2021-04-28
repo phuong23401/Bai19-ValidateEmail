4ee6961ad15dec0d09fd5aa2a3d7ebce00316f2e
phuong23401 <hoaphuong23401@gmail.com>
2021-04-28 15:56:07 +0700

    add

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static final String[] VALID_EMAIL = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] INVALID_EMAIL = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
+
+    public static void main(String args[]) {
+        EmailExample emailExample = new EmailExample();
+        for (String email : VALID_EMAIL) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+        for (String email : INVALID_EMAIL) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+
+    }
+}
